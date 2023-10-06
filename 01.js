f4ab1f0f6aaac60766830a3bc5a63c1156c8ac12
Rohit kumar <126380249+Rohit20140048861@users.noreply.github.com>
2023-10-06 23:52:54 +0530

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,249 @@
+
+//console.log("Hello world")
+// let firstName= "  rohit kumar"
+//  firstName=firstName.toUpperCase();
+// console.log(firstName);
+
+// let num1=BigInt("3645656656");
+// let num2=BigInt("487348748473874843748438");
+// console.log(num1+num2)
+
+// var $name= "Rohit kumar"
+// $name="Mohit Kumar"
+// console.log($name);
+
+// age="34"  
+// age =+age        //string ko number me change kiya
+// console.log(typeof age)
+
+// age=54
+// age =age +""        //number ko string me change kiya
+// console.log(typeof age)
+
+// let myAge;
+// console.log(myAge);
+// var age =null;
+// console.log(typeof age);  here show object bug
+
+// let num1=4
+// let num2=45
+// if(num1>=num2){
+//     console.log("ok")
+// }
+// else{
+//     console.log("badiya")
+// }
+
+// let firstName="Rohit kumar"
+// let age=19                 //template string--> ${} ese use
+// console.log(`Hello my name is ${firstName} and my age is ${age}`)
+
+// let name="Rohti"
+// console.log(name[4])   //string index
+// console.log(name.length)   //string lenght
+
+//methods in string
+
+// let name="     Rohit kumar "
+// let firstName=name.slice(0,6)  //slice--> //rarnge-1 print 
+// console.log(name.slice(3))  //given value range remove print
+// console.log(firstName)
+// let trin=name.trim()     //trim --> //space trim
+// console.log(trin)
+
+// let fName="Rohit kumar"
+// let uCase=fName.toUpperCase()
+// let uCas=fName.toLocaleLowerCase()
+// console.log(uCas)
+// console.log(uCase)
+
+
+// Note -->  string to number change 
+//    +"" --> string ke age +(plus) lga do
+// Note -->  nmberto string change 
+//    +"" -->number ke bad me //+"space"// lga do
+
+// console.log(typeof (2 + ""))
+// console.log(typeof +"3")
+
+
+// let $Name="Rohit "
+// let $lName="kumar"
+// console.log($Name+$lName)  //string concatenation
+
+// console.log(`my name is ${$Name} and my last name is ${$lName} `)  //here we have used templae string rather than simple add on statement
+
+
+
+// let firstName      //variable me value assign nhi ki 
+// console.log(firstName)      //output undefined
+
+// let firstNamee=null     
+// console.log(firstNamee)   //null datatype
+
+// let num1=BigInt(454346565654666656566646)    //using BigInt
+// let num2=5485747894598475874589278437585798n    //used to assign a big num using number ke last me  n
+// console.log(num1
+// console.log(num2)
+
+
+// let num1=7
+// let num2="7"
+// console.log(num1==num2)   //== operator which is always true either the data types are diff
+// console.log(num1===num2)   // === operator need to be true only if data types are same
+// console.log(num1!=num2)  //!= work as logic of ==
+// console.log(num1!==num2)    // !== work as logic of ===
+
+
+// let num = 45
+
+// if (num % 2 === 0) {
+//     console.log("even")   //if else statement
+// } else {
+//     console.log("odd")
+// }
+
+//note ==> falsy and truthy values 
+//falsy-->undefined , nulll, 0, "" false
+//truthy==>abc "sting " number but no 0
+//eg:-
+
+// let firstName=null    // falsy value input 
+// if (firstName){
+//     console.log(firstName)   // so it will not give output 
+// }else{
+//     console.log("Ho")    //only this will print
+// }
+
+
+
+
+// ternery operator ==> condition ? string : strin
+// let age=60;
+// let check=(age>=56 ? "very old" : "you are still young" )  //here we used ternery operator
+// console.log(check)
+
+
+// ana operator && ==>both condition should be true 
+// or operator || ==> any one condition can be true
+
+// if else if else if ....else statement  
+
+
+//switch staement 
+// let day = 3
+// switch (day) {
+//     case 0:
+//     console.log("sunday")
+//     break
+//     case 1:
+//     console.log("monday")
+//     break
+//     case 2:
+//     console.log("tuesday")
+//     break
+//     case 3:
+//     console.log("wednesday")
+//     break
+//     case 4:
+//     console.log("thursday")
+//     break
+//     // .
+//     // .
+//     // .
+//     default:
+//         console.log("invalid day")
+    
+//   }
+
+
+
+//loops 
+//while, do while, for 
+
+// let i=0
+// while (i<=10){           //while loop statement of print 10 numbers
+//     console.log(i)
+//     i++
+// }
+
+
+// do while ==> it runs the code in do block either the condition is false in while block..
+
+// let i=11
+// do{
+//     console.log(i)
+// }while(i<=10)
+
+
+
+
+// wap sum of 10 num
+ 
+// let num=100
+// let i=0
+// let sum=0
+// while (i<=num){        
+//     sum=sum+i
+//     i++
+// }
+// console.log(sum)
+   
+
+// let num=10         
+// console.log(num*(num+1)/2)       //aternate method easy on to print sum using formula
+
+// let num=100
+// let sum=0
+// for (let i=0;i<=num;i++){    //using for loop sum of given numbers
+//     sum=sum+i
+// }
+// console.log(sum)
+
+
+
+// break and continue staement
+// break me whi se exit and continue me wo no print nhi hoga jha se continue hua
+
+// for (let i =0;i<=10;i++){
+   
+//     if (i===3){
+//         break             // break statement
+//     }
+//     else{
+//         console.log(i)
+//     }
+
+// }
+
+
+// for (let i =0;i<=10;i++){
+   
+//     if (i===3){
+//        continue           //continue statement 3 pr ye print nhi hoga and wha se sab continue chalega and print hoga
+
+//     }
+//     else{
+//         console.log(i)
+//     }
+
+// }
+
+
+
+
+                            //Arrays 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
